d6dc6c13d484203639e6ad42910ef1720e8a5f98
silver0a1 <ryan@aperturesec.com>
2016-07-14 00:05:24 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Workspace::Application.config.secret_key_base = '4bb7bfcf629cb5e0a6d540a98bba9206bc17ff74fa8578cc25eff01f6dbdcfb84a976813bcd79dc264fb6ec4c3ed184afc5653a59c2da7ad37fd21dc687ae3ee'
